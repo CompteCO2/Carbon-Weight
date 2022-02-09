161d58e66136ccac1dc3b56bd39a83ebbe697c4e
michael.jeulin-l <michael.jeulin-lagarrigue@compteco2.com>
2022-02-09 09:01:50 +0100

    TYPES: Add house heater types

--- src/house/types.ts
+++ src/house/types.ts
@@ -2,17 +2,20 @@
 export enum ClimateE {
   H1 = "H1", // Cold
   H2 = "H2", // Medium
-  H3 = "H3", // Warm
+  H3 = "H3" // Warm
 }
 
 // Heater Combustible types available
 export enum HeaterE {
-  urban = "urban",        // District Heating - ?
-  electric = "electric",  // Electric - KWh
-  gas = "gas",            // Domestic Gas - Kg
-  GPL = "GPL",            // GPL (Propane-Butane) - Kg
-  fuelOil = "fuelOil",    // Domestic Fuel Oil - L
-  wood = "wood",          // Firewood / Coal - Kg
+  urban = "urban", // District Heating - ?
+  electric = "electric", // Electric - KWh
+  fuelOil = "fuelOil", // Domestic Fuel Oil - L
+  gas = "gas", // Domestic Gas - Kg
+  GPL = "GPL", // GPL (Propane-Butane) - Kg
+  heatPump = "heatPump", // Heat Pump (electric)
+  thermalSolar = "thermalSolar", // Thermal Solar (electric)
+  propane = "Propane", // Propane - Kg
+  wood = "wood" // Firewood / Coal - Kg
 }
 
 // House types available
@@ -23,45 +26,47 @@ export enum HouseE {
 
 // Climate available
 export enum YearE {
-  old = "old",        // Before 1975 (included)
-  recent = "recent"   // After 1975
+  old = "old", // Before 1975 (included)
+  recent = "recent" // After 1975
 }
 
 // Heater factors average informations
 export type ConsumptionI = {
-  emissionFactor: number, // kWh/(m².year)
-  part: number,           // Part of use in %
-  surface: number         // m²
-}
+  emissionFactor: number; // kWh/(m².year)
+  part: number; // Part of use in %
+  surface: number; // m²
+};
 
 // Combustible factors informations
 export type HeaterI = {
-  emissionFactor: number, // kWh/(m².year)
-  energyFactor: number    // kgCO2e/U (combustible unit)
-}
+  emissionFactor: number; // kWh/(m².year)
+  energyFactor: number; // kgCO2e/U (combustible unit)
+};
 
 // House informations
 export type HouseT = {
-  emission?: number, // estimated house emissions in kgCO2/year
-  heater: HeaterE,   // heater type
-  built: YearE,      // Whether the house was built before 1975 (included) or not
-  region?: string,   // region of the house
-  surface: number,   // habitable surface of the house in m2
-  type: HouseE,      // Housing type
-}
+  emission?: number; // estimated house emissions in kgCO2/year
+  heater: HeaterE; // heater type
+  built: YearE; // Whether the house was built before 1975 (included) or not
+  region?: string; // region of the house
+  surface: number; // habitable surface of the house in m2
+  type: HouseE; // Housing type
+};
 
 // Interface of data factors used to compute emission
 export type DataI = {
-  climateCoeffs: {[key in keyof typeof ClimateE]: number},   // Coeff
-  emissionFactors: {[key in keyof typeof HeaterE]: HeaterI}, // kgCO2e/kWh
+  climateCoeffs: { [key in keyof typeof ClimateE]: number }; // Coeff
+  emissionFactors: { [key in keyof typeof HeaterE]: HeaterI }; // kgCO2e/kWh
   consumptionFactors: {
     [key in keyof typeof YearE]: {
       [key in keyof typeof HouseE]: {
-        [key in keyof typeof HeaterE]: ConsumptionI
-  }}},
-  regions: {[key: string]: ClimateE},
+        [key in keyof typeof HeaterE]: ConsumptionI;
+      };
+    };
+  };
+  regions: { [key: string]: ClimateE };
   study: {
-    apartment: number,
-    house: number
-  }
-}
+    apartment: number;
+    house: number;
+  };
+};
