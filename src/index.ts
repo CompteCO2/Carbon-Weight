a760553ed59b35c4454c76114df3f16065a6041e
michael.jeulin-l <michael.jeulin-lagarrigue@compteco2.com>
2022-06-24 17:44:45 +0200

    Export Types

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { default as Flight } from "./flight";
+export { Types as FlightTypes } from "./flight";
+export { default as Food } from "./food";
+export { Types as FoodTypes } from "./food";
+export { default as House } from "./house";
+export { Types as HouseTypes } from "./house";
+export { default as Vehicle } from "./vehicle";
+export { Types as VehicleTypes } from "./vehicle";
